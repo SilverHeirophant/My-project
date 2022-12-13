587b931fae4a35461c80e3623aaf614e2efab1fd
joshua king <jking24@aoiths.org>
2022-12-13 13:25:57 -0500

    added movement and "sneak" speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,16 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
+
 {
+    public float startSpeed = 10.0f;
+    public float speed = 10.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public float turnspeed = 100.0f;
+    float sneak;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Declarations
+       
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //move character
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            sneak = speed / 2;
+            speed = sneak;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+        {
+            speed = startSpeed;
+        }
+
         
+
+
     }
+
 }
